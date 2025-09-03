a72ed848c8c8f6ecd6b88baad0349a8fab2342d0
mgouveas <mgsandmgs@gmail.com>
2025-09-02 22:14:55 -0300

    chore: Iniciando projeto

---
+++ init/schema.sql
@@ -0,0 +1,24 @@
+-- Criação da tabela de eventos
+CREATE TABLE Eventos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+);
+
+-- Criação da tabela de participantes
+CREATE TABLE Participantes (
+    id INTEGER PRIMERY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    email TEXT NOT NULL,
+    link TEXT,
+    evento_id INTEGER NOT NULL,
+    FOREIGN KEY (evento_id) REFERENCES Eventos(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- Criação da tebaa de Eventos_link
+CREATE TABLE Eventos_link (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    evento_id INTEGER NOT NULL,
+    participante_id INTEGER NOT NULL,
+    FOREIGN KEY (evento_id) REFERENCES Eventos(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (participante_id) REFERENCES Participantes(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
