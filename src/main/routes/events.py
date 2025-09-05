d31f7575c096b4cd2604b9f340fcd44d587e254d
mgouveas <mgsandmgs@gmail.com>
2025-09-05 00:02:12 -0300

    feat: implementando os controllers e criando a rota de criacao de participantes

--- src/main/routes/events.py
+++ src/main/routes/events.py
@@ -1,9 +1,19 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from src.http_types.http_request import HttpRequest
+from src.validators.events_creator_validator import events_creator_validator
+from src.controllers.events.event_creator import EventsCreator
+from src.model.repositories.eventos_repository import EventosRepository
 
 event_route_bp = Blueprint("event_route", __name__)
 
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
-    return jsonify({
-        "message": "New event created successfully"
-    }), 201
+    events_creator_validator(request)
+    http_request = HttpRequest(body=request.json or {})
+
+    eventos_repo = EventosRepository()
+    events_creator = EventsCreator(eventos_repo)
+
+    http_response = events_creator.create(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
